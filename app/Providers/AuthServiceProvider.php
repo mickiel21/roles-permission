2dae11f2f598fd6f07cf41443a33a0d8d6cb2c5d
Miko <mickieloraa21@gmail.com>
2023-05-24 14:28:27 +0800

    Creating seeder file for Roles and Permissions / Dummy Post

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,21 +1,18 @@
 <?php
-
 namespace App\Providers;
-
+// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
+     * The model to policy mappings for the application.
      *
      * @var array<class-string, class-string>
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
-
     /**
      * Register any authentication / authorization services.
      *
@@ -24,7 +21,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        // Implicitly grant "Super-Admin" role all permission checks using can()
+        Gate::before(function ($user, $ability) {
+            if ($user->hasRole(env('APP_SUPER_ADMIN', 'super-admin'))) {
+                return true;
+            }
+        });
     }
 }
